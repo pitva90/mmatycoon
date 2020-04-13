9c316d59a9da563787efff483e1e3a80fbe37299
pitva90 <46487018+pitva90@users.noreply.github.com>
2020-04-13 08:18:02 +0200

    Create user-script.js

---
+++ user-script.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         MMA Tycoon script
+// @namespace    http://www.mmatycoon.com/
+// @version      0.1
+// @description  Make it better!
+// @author       Pitva
+// @match        http://www.mmatycoon.com/*
+// @grant        none
+// @require https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+$('a:contains("Spoiler / News")').after('<br><a style="color:#ff0000; font-weight: 700;" href="/spoilerfight.php">Spoiler Fight</a>');
+
+$('#fight').css('font-size','16px');
+
+$('td').each(function() {
+    tdWidth = $(this).attr("width");
+    tdWidthRounded = parseFloat(tdWidth).toFixed(2);
+    bgImage = $(this).css('background-image');
+    console.log(bgImage);
+    if($(this).css('background-image').toString().indexOf("bar") > -1)  {
+        if($(this).css('background-image').toString().indexOf(".jpg") > -1)  {
+        $(this).css("position","relative");
+        $(this).append('<span style="position: absolute; left: 5px; font-weight: 600;"></span>');
+        $(this).find('span').html(tdWidthRounded + '%');
+        };
+    };
+});
